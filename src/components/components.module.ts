d1394b3f6961f42ecb8c48ae9d3c2023e5179207
Aaron Barboza <abarboza@ticopointer.com>
2018-08-05 20:15:46 -0600

    Commit and push test

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { SettingsComponent } from './settings/settings';
+@NgModule({
+	declarations: [SettingsComponent],
+	imports: [],
+	exports: [SettingsComponent]
+})
+export class ComponentsModule {}
